0483acc576b7cceda9dda2704cb91a3f3db929d1
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-03 11:23:50 -0500

    Crear función para leer archivo markdown y convertirlo a HTML

--- server/Controlador_de_Archivos.js
+++ server/Controlador_de_Archivos.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { marked } = require('marked');
 
 const directorioMarkdown =path.join(__dirname, 'markdowns');
 
@@ -8,3 +9,10 @@ function listarArchivos() {
 }
 
 console.log(listarArchivos())
+
+function leerArchivoComoHTML(nombreArchivo) {
+    const rutaArchivo = path.join(directorioMarkdown, nombreArchivo);
+    if (!fs.existsSync(rutaArchivo)) return null;
+    const markdown = fs.readFileSync(rutaArchivo, 'utf-8');
+    return marked(markdown);
+}

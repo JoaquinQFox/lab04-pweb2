71815714174b6592fa24558d05fc82729b3b82bb
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-03 11:14:51 -0500

    Crear función para listar archivos markdown

--- server/Controlador_de_Archivos.js
+++ server/Controlador_de_Archivos.js
@@ -2,3 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const directorioMarkdown =path.join(__dirname, 'markdowns');
+
+function listarArchivos() {
+    return fs.readdirSync(directorioMarkdown).filter(archivo => archivo.endsWith('.md'))
+}
+
+console.log(listarArchivos())

bbffe986c1110afd3ec82b58d27bd11957e39d22
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-03 12:28:06 -0500

    implemente una ruta para que nos muestre el contenido del archivo con la funcion leer archivoComoHTML

--- server/rutas.js
+++ server/rutas.js
@@ -6,3 +6,13 @@ rutas.get('/archivos', (solicitud,respuesta) => {
     const archivos = listarArchivos();
     respuesta.json({archivos});
 });
+
+rutas.get('/archivos/:nombreArchivo', (solicitud,respuesta) => {
+    const nombreArchivo = solicitud.params.nombreArchivo;
+    const contenidoHTML = leerArchivoComoHTML(nombreArchivo);
+    if (contenidoHTML) {
+        respuesta.send(contenidoHTML);
+    } else {
+        respuest.status(404).send('Archivo no encontrado');
+    }
+});

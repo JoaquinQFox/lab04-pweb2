61ed58353274303212962ff34a4e2fac6e099c55
leonhatches <jhatches@unsa.edu.pe>
2025-05-04 12:17:51 -0500

    | app.js |: Se crea una función para listar los archivos - Error: No muestra ninguno

--- pub/app.js
+++ pub/app.js
@@ -5,6 +5,17 @@ function cargarArchivos () {
 	fetch ('/api/archivos')
 		.then(respuesta => respuesta.json())
 		.then(data => {
-			console.log(data);
+			listarArchivos(data);
 		})
+}
+
+function listarArchivos(data) {
+	let lista = "<ul>";
+
+	for (let i = 0 ; i < data.length ; i++) {
+		lista += "<li><a>" + data[i] + "</a></li>";
+	}
+
+	lista += "</ul>";
+	document.getElementById("listaDiv").innerHTML = lista;
 }

e5e1e447cd996e2242b202031882bbe061a319dd
leonhatches <jhatches@unsa.edu.pe>
2025-05-04 14:55:48 -0500

    | app.js |: Se crea la función para cargar un Archivo y mostrar en consola - Error en la parte de rutas

--- pub/app.js
+++ pub/app.js
@@ -10,11 +10,21 @@ function cargarArchivos () {
 }
 
 function listarArchivos(data) {
+	
 	let lista = "";
 
 	for (let i = 0 ; i < data.archivos.length ; i++) {
-		lista += "<li><a>" + data.archivos[i] + "</a></li>";
+		lista += "<li><a onclick='cargarArchivo(\""+ data.archivos[i] +"\")'>" + data.archivos[i] + "</a></li>";
 	}
 
 	document.getElementById("listaDiv").innerHTML = lista;
+}
+
+function cargarArchivo (nombreArchivo) {
+	console.log(nombreArchivo);
+	fetch ("/api/archivos/"+nombreArchivo)
+		.then(respuesta => respuesta.json())
+		.then(data => {
+			console.log(data);
+		});
 }
